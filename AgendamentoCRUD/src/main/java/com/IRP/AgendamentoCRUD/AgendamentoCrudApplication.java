2a3867cc8ef746893f36c84bf591213411ff08ca
Italo-Ribeiro-Prates <italoribeiroprates@gmail.com>
2026-02-11 19:40:44 -0300

    commit inicial

---
+++ AgendamentoCRUD/src/main/java/com/IRP/AgendamentoCRUD/AgendamentoCrudApplication.java
@@ -0,0 +1,13 @@
+package com.IRP.AgendamentoCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoCrudApplication.class, args);
+	}
+
+}
---
+++ AgendamentoCRUD/src/test/java/com/IRP/AgendamentoCRUD/AgendamentoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IRP.AgendamentoCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
